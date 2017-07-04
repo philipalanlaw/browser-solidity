f0cd51b00f04bee3acbb8e941a14937fb1a42c25
yann300 <yann.levreau@gmail.com>
2017-07-04 13:48:55 +0200

    move code away from universal dapp

--- src/app.js
+++ src/app.js
@@ -558,9 +558,6 @@ function run () {
     resetDapp: (udappContracts, renderOutputModifier) => {
       udapp.reset(udappContracts, transactionContextAPI, renderOutputModifier)
     },
-    renderDapp: () => {
-      return udapp.render()
-    },
     getAccounts: (callback) => {
       udapp.getAccounts(callback)
     },
@@ -675,6 +672,15 @@ function run () {
     },
     executionContextProvider: () => {
       return executionContext.getProvider()
+    },
+    getContracts: () => {
+      return compiler.lastCompilationResult.data.contracts
+    },
+    udapp: () => {
+      return udapp
+    },
+    executionContext: () => {
+      return executionContext
     }
   }
   var rhpEvents = {
